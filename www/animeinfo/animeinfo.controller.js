d386fc5e7f7bafc36c0e1eadb134fb8cfa9afc61
cyalins <Marcus@Marcuss-Air.gateway>
2015-09-05 13:53:59 +1000

    Added Library Entry modal

--- www/animeinfo/animeinfo.controller.js
+++ www/animeinfo/animeinfo.controller.js
@@ -1,8 +1,24 @@
 angular.module('starter')
-.controller('AnimeInfoCtrl', function($scope, $state, $stateParams, ionicMaterialInk, ionicMaterialMotion, Animes, DbService) {
+.controller('AnimeInfoCtrl', function($scope, $state, $stateParams, $ionicModal, ionicMaterialInk, ionicMaterialMotion, Animes, DbService) {
   
   $stateParams.animeId == undefined;
   ionicMaterialInk.displayEffect();
   $scope.anime = DbService.get($stateParams.animeId);
   console.log($stateParams.animeId);
+
+
+  //editor
+  $ionicModal.fromTemplateUrl('editanime.html', function(modal) {
+    $scope.editModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-right'
+  });
+
+  $scope.openAnimeEdit = function(animeId) {
+  	$scope.editModal.show();
+  };
+  $scope.closeAnimeEdit = function() {
+  	$scope.editModal.hide();
+  };
 });
